f9f5dda4ad8ee6102df6ef42d78373dd9d93b8a3
htbkoo <htbkoo@users.noreply.github.com>
2022-04-15 00:23:59 +0000

    deploy: 138e57648fd056187b30a82a43afa99d67a2d64c

--- _next/static/KzldOQp2zMdbAYuxvVC7R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/40-94669502d7b54da1.js","static/chunks/pages/index-a380b09497c7f47a.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
