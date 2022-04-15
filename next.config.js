919e537bf2552461a4e7e11fa8e35aff9a729c56
HeySP6 <h1017760@connect.hku.hk>
2022-04-15 17:20:06 +0200

    fetched changes from upstream (https://github.com/htbkoo/personal-portfolio)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 // reference: https://github.com/cyrilwanner/next-optimized-images#installation
-const withOptimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
+// reference:
+// 1. https://github.com/vercel/next.js/blob/canary/examples/progressive-web-app/next.config.js
+// 2. https://github.com/shadowwalker/next-pwa/issues/252
+const pwa = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 // reference: https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
 const isProd = process.env.NODE_ENV === "production";
 
 // reference: https://blog.sallai.me/deploy-next-site-to-github-pages
-const basePath = isProd ? "/personal-portfolio-staging" : "";
+const basePath = isProd ? "/personal-portfolio" : "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -18,7 +25,7 @@ const nextConfig = {
 
     // reference: https://github.com/cyrilwanner/next-optimized-images/issues/272
     images: {
-        loader: 'custom',
+        loader: "custom",
         // reference: https://stackoverflow.com/a/68012194
         disableStaticImages: true,
     },
@@ -28,4 +35,29 @@ const nextConfig = {
     },
 };
 
-module.exports = withOptimizedImages(nextConfig);
+module.exports = withPlugins([
+    [
+        optimizedImages,
+        {
+            /* config for next-optimized-images */
+        },
+    ],
+
+    // your other plugins here
+    [
+        pwa,
+        {
+            pwa: {
+                dest: `public`,
+                runtimeCaching,
+                // Don't precache files under .next/static/chunks/images (Highly recommend this to work with next-optimized-images plugin)
+                // reference: https://github.com/shadowwalker/next-pwa#available-options
+                buildExcludes: [/chunks\/images\/.*$/],
+                // recommend: set to false if your start url always returns same HTML document, then start url will be precached, this will help to speed up first load.
+                dynamicStartUrl: false,
+            },
+        },
+    ],
+
+    nextConfig,
+]);
